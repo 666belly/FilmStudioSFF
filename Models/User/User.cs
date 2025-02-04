a2e9290e1a51c01051deca986fa5e8f02cbb116a
Isabel Demirel <isabeldemirel@MacBook-Pro-2.local>
2025-02-04 13:33:40 +0100

    IUser added

--- Models/User/User.cs
+++ Models/User/User.cs
@@ -5,8 +5,8 @@ namespace FilmStudioSFF.Models
     public class User
     {
         public int UserId { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-        public bool IsAdmin { get; set; } //admin or filmstudio
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+        public required string Role { get; set; } //admin or filmstudio
     }
 }
