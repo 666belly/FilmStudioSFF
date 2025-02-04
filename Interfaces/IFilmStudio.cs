888222f1a61cf435ae65d2478cd6b6c62e05ceb4
Isabel Demirel <isabeldemirel@MacBook-Pro-2.local>
2025-02-04 12:59:16 +0100

    IRegisterFilmStudio added

--- Interfaces/IFilmStudio.cs
+++ Interfaces/IFilmStudio.cs
@@ -6,5 +6,6 @@ public interface IFilmStudio
     int FilmStudioId { get; set; }
     string Name { get; set; }   
     string City { get; set; }   
+    public string Email { get; set; }
     List<FilmCopy> RentedFilmCopies { get; set; }
 }
--- Models/FilmStudio/FilmStudio.cs
+++ Models/FilmStudio/FilmStudio.cs
@@ -7,6 +7,7 @@ namespace FilmStudioSFF.Models
         public int FilmStudioId { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
+        public string Email { get; set; }
         public List<FilmCopy> RentedFilmCopies { get; set; }
     }
 
