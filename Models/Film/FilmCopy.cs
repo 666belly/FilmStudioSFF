6772cc4f5d57d317d4fc35d863dff2d4533ad132
Isabel Demirel <isabeldemirel@MacBook-Pro-2.local>
2025-02-04 12:12:27 +0100

    FilmCopy properties added

---
+++ Models/Film/FilmCopy.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace FilmStudioSFF.Models
+{
+    public class FilmCopy
+    {
+        public int FilmCopyId { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
